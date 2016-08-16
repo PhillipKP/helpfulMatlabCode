b439be152d529262bf57fdf86aada7424317779b
Phillip K. Poon <ppoon@optics.arizona.edu>
2016-08-16 18:16:24 -0400

    Add files via upload
    This MATLAB function computes the L2 Norm of the columns of a matrix.

---
+++ l2NormOfCol.m
@@ -0,0 +1,10 @@
+function [normList] = l2NormOfCol(M)
+% normList = l2NormOfCol(M)
+% This is used to compute the L2 Norm of each column in a matrix.
+% 
+% Phillip K Poon 16 Aug 2016
+
+normList = diag(sqrt(M'*M));
+
+
+end
